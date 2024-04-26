5d0ee99234d99cd8a64cf2ba8b40c3241edfd045
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-26 14:52:31 -0300

    feat: create app nav host

---
+++ app/src/main/java/br/com/dio/soccernews/ui/components/bottom_nav/AppNavHost.kt
@@ -0,0 +1,34 @@
+package br.com.dio.soccernews.ui.components.bottom_nav
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import br.com.dio.soccernews.ui.favorites.FavoritesScreen
+import br.com.dio.soccernews.ui.favorites.FavoritesViewModel
+import br.com.dio.soccernews.ui.news.NewsScreen
+import br.com.dio.soccernews.ui.news.NewsViewModel
+
+@Composable
+fun AppNavHost(navController: NavController, modifier: Modifier) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = Screen.News.route,
+        modifier = modifier
+    ) {
+
+        composable(Screen.News.route) {
+            val newsViewModel = hiltViewModel<NewsViewModel>()
+            NewsScreen(newsViewModel = newsViewModel)
+        }
+
+        composable(Screen.Favorites.route) {
+            val favoritesViewModel = hiltViewModel<FavoritesViewModel>()
+            FavoritesScreen(favoritesViewModel = favoritesViewModel)
+        }
+
+    }
+}
