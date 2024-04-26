3592a1f894a3b2ad58ae416a2f026abe3a0665a6
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-25 23:01:13 -0300

    feat: news list composable

---
+++ app/src/main/java/br/com/dio/soccernews/ui/components/NewsList.kt
@@ -0,0 +1,46 @@
+package br.com.dio.soccernews.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.dio.soccernews.domain.model.News
+
+@Composable
+fun NewsList(newsList: List<News>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(newsList) { news ->
+            NewsItem(news = news)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NewsListPreview() {
+
+    val newsList = listOf(
+        News(
+            id = 1,
+            title = "Você sabia?",
+            headline = "Mulheres colecionam conquistas históricas no futebol; veja oito delas neste 8 de março.",
+            image = "https://rodolfohok.github.io/dio.soccer-news-api/images/1.jpg",
+            link = "https://ge.globo.com/pe/futebol/noticia/2022/03/08/voce-sabia-mulheres-colecionam-conquistas-historicas-no-futebol-veja-oito-delas-neste-8-de-marco.ghtml",
+            favorite = false
+        ),
+        News(
+            id = 2,
+            title = "Por que vou parar de sonhar?",
+            headline = "Em carta ao filho, Cristiane recorda infância dura e ambições: Por que vou parar de sonhar?",
+            image = "https://rodolfohok.github.io/dio.soccer-news-api/images/2.jpg",
+            link = "https://ge.globo.com/pe/futebol/noticia/2022/03/02/em-carta-ao-filho-cristiane-recorda-infancia-dura-e-ambicoes-por-que-vou-parar-de-sonhar.ghtml",
+            favorite = true
+        ),
+    )
+
+    NewsList(newsList = newsList)
+}
