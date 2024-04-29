1ebbe80da8cbd5144763ad0ae3a2cd6a1742e2f6
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-28 22:30:27 -0300

    refactor: remove unused dependencies after jetpack compose migration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,37 +45,13 @@ android {
 }
 
 dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2024.04.01")
+    val nav_version = "2.7.7"
     val retrofit_version = "2.11.0"
     val room_version = "2.6.1"
     val hilt_version = "2.51.1"
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-
-    implementation(libs.androidx.swiperefreshlayout)
-
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    implementation("com.google.dagger:hilt-android:$hilt_version")
-
-    kapt("androidx.room:room-compiler:$room_version")
-    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    // Jetpack Compose Migration
-    val composeBom = platform("androidx.compose:compose-bom:2024.04.01")
-    val nav_version = "2.7.7"
 
     implementation(composeBom)
     implementation("androidx.compose.material3:material3")
@@ -85,13 +61,23 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
     implementation("androidx.navigation:navigation-compose:$nav_version")
 
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation("com.google.dagger:hilt-android:$hilt_version")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("com.squareup.picasso3:picasso-compose:3.0.0-alpha05") // substitute com.squareup.picasso:picasso
+    implementation("com.squareup.picasso3:picasso-compose:3.0.0-alpha05")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    kapt("androidx.room:room-compiler:$room_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
 
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(composeBom)
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 }
